330cb41ebc759ded3ad13d58b3b186c842a22aed
Reginaldo Moura <reginaldo.mourap@gmail.com>
2024-01-30 17:00:51 -0400

    fix: declare variable from home route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -14,6 +14,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
+    public const HOME = '/';
+
     protected $namespace = 'App\Http\Controllers';
 
     public function map(): void
@@ -21,7 +23,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->mapApiRoutes();
     }
 
-    protected function mapApiRoutes()
+    protected function mapApiRoutes(): void
     {
         Route::middleware('api')
             ->namespace($this->namespace)
