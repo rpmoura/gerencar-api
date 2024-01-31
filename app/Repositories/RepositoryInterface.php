0bc903d499733b466252a2a865f42c2b556c859d
Reginaldo Moura <reginaldo.mourap@gmail.com>
2024-01-31 10:17:06 -0400

    feat: add abstract repository for eloquent models

---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+interface RepositoryInterface
+{
+    public function findBy(string $key, mixed $value, array $columns = ['*']);
+
+    public function findOneBy(string $key, mixed $value);
+
+    public function create(array $attributes);
+
+    public function update(array $attributes, int $id);
+
+    public function paginate(int $limit = null, array $columns = ['*'], string $method = "paginate");
+
+    public function all(array $columns = ['*']);
+
+    public function get(array $columns = ['*']);
+
+    public function delete(int $id);
+
+    public function sync(int $id, string $relation, array $attributes, bool $detaching = true);
+}
