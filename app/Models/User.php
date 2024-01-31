cc4adc11dd00a7548f18c34df8dfc1977886be28
Reginaldo Moura <reginaldo.mourap@gmail.com>
2024-01-31 11:21:22 -0400

    chore: remove unsed classes and fields from user model.
    Remove Authenticatable, Notifiable and unsed fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
     use HasApiTokens;
     use HasFactory;
-    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -32,7 +29,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -41,7 +37,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password'          => 'hashed',
+        'password' => 'hashed',
     ];
 }
