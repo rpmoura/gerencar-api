44fed3c1cf12b2d0a0d005510ba632c179e3ce99
Reginaldo Moura <reginaldo.mourap@gmail.com>
2024-01-31 20:15:11 -0400

    chore: update user-related messages

--- lang/en/exception.php
+++ lang/en/exception.php
@@ -3,4 +3,7 @@
 return [
     'method_not_allowed' => 'Method not allowed',
     'internal_error'     => 'Wrong... It\'s too bad we had a technical problem.',
+    'user'               => [
+        'not_found' => 'User not found',
+    ],
 ];
---
+++ lang/en/message.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'user' => [
+        'listed_successfully'  => 'Users listed successfully',
+        'created_successfully' => 'User created successfully',
+        'updated_successfully' => 'User updated successfully',
+        'deleted_successfully' => 'User deleted successfully',
+    ],
+];
